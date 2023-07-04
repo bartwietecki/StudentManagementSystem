3d30f4e764ea840e1478799bd27f5ab04f6e8828
Bart <bartwietecki@gmail.com>
2023-07-04 09:57:06 +0200

    Creating and updating Student class

---
+++ src/main/java/net/javaguides/sms/entity/Student.java
@@ -0,0 +1,28 @@
+package net.javaguides.sms.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+}
